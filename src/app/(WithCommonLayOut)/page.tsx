da7294d55f3b452a00abbf71f4e900cbb95aff9e
al-af-muktadir <al.afmuktadir.28@gmail.com>
2025-03-27 13:48:40 +0600

    sasd

---
+++ src/app/(WithCommonLayOut)/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Blocks/Navbar";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default HomePage;
--- src/components/Blocks/Navbar.tsx
+++ src/components/Blocks/Navbar.tsx
@@ -1,6 +1,10 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
+import asylum from "../../assets/logo-transparent.png";
+import { FaFolder, FaHome, FaList, FaListUl, FaUsers } from "react-icons/fa";
+import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
 
 export default function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -8,47 +12,50 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-900 text-white p-4 shadow-lg border-b-2 border-[#396C2F]">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-[#A3C664]">Futuristic Rent</h1>
-        <ul className="flex space-x-6">
-          <li>
-            <Link href="/" className="hover:text-[#A3C664] transition">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/about" className="hover:text-[#A3C664] transition">
-              About Us
-            </Link>
-          </li>
-          <li>
-            <Link href="/listings" className="hover:text-[#A3C664] transition">
-              All Rentals
-            </Link>
-          </li>
-          <li>
-            <Link href="/dashboard" className="hover:text-[#A3C664] transition">
+        <div className="text-[#A3C664] font-bold">AsyLum </div>
+        <div className="flex space-x-10">
+          <Link
+            href="/"
+            className="hover:text-[#A3C664] flex items-center   transition"
+          >
+            <FaHome /> Home
+          </Link>
+
+          <Link
+            href="/about"
+            className="hover:text-[#A3C664]  gap-1 flex items-center transition"
+          >
+            <FaUsers /> About
+          </Link>
+
+          <Link
+            href="/listings"
+            className="hover:text-[#A3C664] gap-1 flex items-center transition"
+          >
+            <HiOutlineClipboardDocumentList /> All Rentals
+          </Link>
+        </div>
+        <div className="space-x-6">
+          <Link href="/dashboard" className="hover:text-[#A3C664] transition">
+            <button className="px-2 py-2 rounded-2xl hover:text-[#A3C664] transition border-2">
               Dashboard
-            </Link>
-          </li>
+            </button>
+          </Link>
+
           {isLoggedIn ? (
             <>
-              <li>
-                <Link
-                  href="/profile"
-                  className="hover:text-[#A3C664] transition"
-                >
-                  My Profile
-                </Link>
-              </li>
+              <Link href="/profile" className="hover:text-[#A3C664] transition">
+                My Profile
+              </Link>
             </>
           ) : (
-            <li>
-              <Link href="/auth" className="hover:text-[#A3C664] transition">
-                Login / Register
-              </Link>
-            </li>
+            <Link href="/login" className="hover:text-[#A3C664] transition">
+              <button className="px-2 py-2 rounded-2xl hover:text-[#A3C664] transition border-2">
+                Login
+              </button>
+            </Link>
           )}
-        </ul>
+        </div>
       </div>
     </nav>
   );
